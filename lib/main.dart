ea5564e52f4ea1c3f7bf25d13877845aee118d84
romajc84 <joeyroman84@gmail.com>
2020-06-09 20:28:57 -0400

    end of free course

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_covid19_tracker/app/services/api.dart';
+import 'package:flutter_covid19_tracker/app/services/api_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _accessToken = '';
+  int _cases;
+  int _deaths;
+
+  void _updateAccessToken() async {
+    final apiService = APIService(API.sandbox());
+    final accessToken = await apiService.getAccessToken();
+    final cases = await apiService.getEndpointData(
+        accessToken: accessToken, endpoint: Endpoint.cases);
+    final deaths = await apiService.getEndpointData(
+        accessToken: accessToken, endpoint: Endpoint.deaths);
+    setState(() {
+      _accessToken = accessToken;
+      _cases = cases;
+      _deaths = deaths;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_accessToken',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            if (_cases != null)
+            Text(
+              'cases: $_cases',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            if (_deaths != null)
+            Text(
+              'deaths: $_deaths',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateAccessToken,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
